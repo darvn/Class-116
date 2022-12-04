aee7be7b04b03d5e067dee02088abd3857fea8e9
darvn <86173198+darvn@users.noreply.github.com>
2022-12-03 19:52:22 -0700

    Add files via upload

---
+++ pythoncode.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def greeting():
+    return "Hello World!"
+
+@app.route("/flask2")
+
+def fla():
+    return "Nice to meet you!"
+
+@app.route("/firstpage")
+
+def firstPage():
+    name = "Darvn"
+    return render_template("index.html", index_variable = name)
+
+app.run(debug = True)
+
